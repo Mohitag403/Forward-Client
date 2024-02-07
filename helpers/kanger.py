17741d72edc3804e9fcd1b2ef264413b4778a833
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-07 17:15:17 +0530

    Update kanger.py

--- helpers/kanger.py
+++ helpers/kanger.py
@@ -8,29 +8,35 @@ from pyrogram.errors import UserDeactivatedBan
 from helpers.forwarder import ForwardMessage
 
 
+
 async def Kanger(c: Client, m: Message):
-    await m.edit(text=f"Checking `{str(Config.FORWARD_FROM_CHAT_ID[0])}` ...")
-    await asyncio.sleep(2)
     try:
-        ForwardFromChat = await c.get_chat(chat_id=Config.FORWARD_FROM_CHAT_ID[0])
-        await m.edit(text=f"Successfully Linked with `{ForwardFromChat.title}` !")
+        forward_from_chat = await c.get_chat(chat_id=Config.FORWARD_FROM_CHAT_ID[0])
+        await m.edit(text=f"Successfully linked with `{forward_from_chat.title}`!")
     except Exception as err:
-        await m.edit(text=f"Sorry, can't get **Forward From Chat**!\n\n**Error:** `{err}`")
-        return 400
-    await asyncio.sleep(2)
-    for i in range(len(Config.FORWARD_TO_CHAT_ID)):
-        await m.edit(text=f"Checking `{Config.FORWARD_TO_CHAT_ID[i]}` ...")
-    await asyncio.sleep(2)
-    await m.edit(text="Trying to Forward Now ...")
+        await m.edit(text=f"Sorry, can't get the forward from chat!\n\nError: `{err}`")
+        return
+
+    for chat_id in Config.FORWARD_TO_CHAT_ID:
+        try:
+            forward_to_chat = await c.get_chat(chat_id=chat_id)
+            if not forward_to_chat.permissions.can_send_messages:
+                await m.edit(text=f"Sorry, you don't have permission to send messages in {forward_to_chat.title}!")
+                return
+            await m.edit(text=f"Successfully linked with `{forward_to_chat.title}`!")
+        except Exception as err:
+            await m.edit(text=f"Sorry, can't get the forward to chat!\n\nError: `{err}`")
+            return
+
+    await m.edit(text="Trying to forward now...")
     async for message in c.iter_history(chat_id=Config.FORWARD_FROM_CHAT_ID[0], reverse=True):
         await asyncio.sleep(Config.SLEEP_TIME)
         try:
-            try_forward = await ForwardMessage(c, message)
-            if try_forward == 400:
-                return 400
+            await ForwardMessage(c, message)
         except UserDeactivatedBan:
-            print("Congratulations!\nYour Account Banned Successfully!\nI already told you use a Fake Account. Hope you remember.")
+            await c.send_message(chat_id="me", text="Your account has been banned!")
             break
         except Exception as err:
             await c.send_message(chat_id="me", text=f"#ERROR: `{err}`")
-    await m.edit(text="Channel Files Successfully Kanged!\n\n©️ A Forwarder Userbot by @AbirHasan2005")
+
+    await m.edit(text="Channel files successfully kanged!\n\n©️ A Forwarder Userbot by @AbirHasan2005")
